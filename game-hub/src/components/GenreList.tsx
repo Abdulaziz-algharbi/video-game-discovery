f54e620943a34bc62f5d845435fa170179f74d8b
abdulaziz <abdulazizalgharbi8@gmail.com>
2025-12-10 20:41:27 +0400

    Filter Games by Genre

--- game-hub/src/components/GenreList.tsx
+++ game-hub/src/components/GenreList.tsx
@@ -1,9 +1,21 @@
-import { HStack, List, ListItem, Image, Text, Spinner } from "@chakra-ui/react";
-import useGenres from "../hooks/useGenres";
+import {
+  HStack,
+  List,
+  ListItem,
+  Image,
+  Text,
+  Spinner,
+  Button,
+} from "@chakra-ui/react";
+import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 import GenreSkeleton from "./GenreSkeleton";
 
-const GenreList = () => {
+interface Props {
+  onSelectGenre: (genre: Genre) => void;
+}
+
+const GenreList = ({ onSelectGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
   const skeletons = [...Array(20)].map((_, i) => i + 1);
   if (error) return null;
@@ -25,7 +37,18 @@ const GenreList = () => {
               borderRadius={8}
               src={getCroppedImageUrl(genre.image_background)}
             />
-            <Text fontSize="lg">{genre.name}</Text>
+            <Button
+              onClick={() => onSelectGenre(genre)}
+              variant="link"
+              fontSize="clamp(12px, 2vw, 18px)"
+              maxW="150px"
+              whiteSpace="nowrap"
+              overflow="hidden"
+              textOverflow="ellipsis"
+              textAlign="left"
+            >
+              {genre.name}
+            </Button>
           </HStack>
         </ListItem>
       ))}
--- game-hub/src/hooks/useGames.ts
+++ game-hub/src/hooks/useGames.ts
@@ -1,4 +1,5 @@
 import useData from "./useData";
+import { Genre } from "./useGenres";
 
 export interface Platform {
   id: number;
@@ -14,6 +15,13 @@ export interface Game {
   metacritic: number;
 }
 
-const useGames = () => useData<Game>("/games?page_size=40");
+const useGames = (selectedGenre: Genre | null) =>
+  useData<Game>(
+    "/games?page_size=40",
+    {
+      params: { genres: selectedGenre?.id },
+    },
+    [selectedGenre?.id]
+  );
 
 export default useGames;
